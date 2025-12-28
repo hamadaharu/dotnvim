207fbc87587184e625afd662cd4dff47078b25db
Hamadah <muhammadrozaan@gmail.com>
2025-12-28 07:32:22 +0700

    feat(images): reformating plugins file and disable hologram

--- lua/plugins/images.lua
+++ lua/plugins/images.lua
@@ -1,29 +1,26 @@
 return {
-  {
-    "edluffy/hologram.nvim",
-    enabled = false,
-  },
-	{
-		"3rd/image.nvim",
-		dependencies = {
-			{
-				"vhyrro/luarocks.nvim",
-				priority = 1001, -- this plugin needs to run before anything else
-				opts = {
-					rocks = { "magick" },
-				},
-			},
-		},
-    ft = {
-      -- "md",
-      -- "markdown",
-      -- "markdown_inline"
+    {
+        "edluffy/hologram.nvim",
+        enabled = false,
     },
-    cmd = {
-      "LoadImagesPls"
+    {
+        "3rd/image.nvim",
+        dependencies = {
+            {
+                "vhyrro/luarocks.nvim",
+                priority = 1001, -- this plugin needs to run before anything else
+                opts = {
+                    rocks = { "magick" },
+                },
+            },
+        },
+        ft = {
+            -- "md",
+            -- "markdown",
+            -- "markdown_inline"
+        },
+        config = function()
+            -- ...
+        end,
     },
-		config = function()
-			-- ...
-		end,
-	},
 }
