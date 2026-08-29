ccf2693fef7ab3e75e2f1836fa71b9928bac8235
Hamadah <muhammadrozaan@gmail.com>
2026-08-29 07:57:39 +0700

    feat(treesitter): add filetype exclusion list for indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,12 +5,20 @@ return {
   cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo", "TSUpdate" },
   build = ":TSUpdate",
   init = function()
+    local disabled_indent_ft = {
+      cs = true,
+      csharp = true
+    }
+
     vim.api.nvim_create_autocmd('FileType', {
-      callback = function() 
+      callback = function(args)
         -- Enable treesitter highlighting and disable regex syntax
-        pcall(vim.treesitter.start)
-        -- Enable treesitter-based indentation
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        pcall(vim.treesitter.start, args.buf)
+
+        -- Enable treesitter-based indentation except for specific filetypes
+        if not disabled_indent_ft[vim.bo[args.buf].filetype] then
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
       end,
     })
   end,
