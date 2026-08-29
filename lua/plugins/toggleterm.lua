750cd041ef13e0da5bec7c2e4a304233f8bb9ae7
Hamadah <muhammadrozaan@gmail.com>
2026-08-29 07:54:38 +0700

    feat(toggleTerm): add terminal with direction

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -9,6 +9,24 @@ return {
         insert_mappings = false,  -- whether or not the open mapping applies in insert mode
         terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
       })
+      
+      local Terminal = require('toggleterm.terminal').Terminal
+
+      local horizontal = Terminal:new({
+        direction = 'horizontal',
+          on_open = function (term) term:resize(12) end
+      })
+
+      local vertical = Terminal:new({
+        direction = 'vertical',
+          on_open = function (term) term:resize(80) end
+      })
+
+      local float = Terminal:new({ direction = 'float' })
+
+      vim.keymap.set({"n", "t"}, '<leader>th', function() horizontal:toggle() end, { desc = "Toggle Term Horizontal" })
+      vim.keymap.set({"n", "t"}, '<leader>tv', function() vertical:toggle() end, { desc = "Toggle Term Vertical" })
+      vim.keymap.set({"n", "t"}, '<leader>tf', function() float:toggle() end, { desc = "Toggle Term Float" })
     end,
   },
 
